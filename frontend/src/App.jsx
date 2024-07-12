0779b1579a196e2f87dc3296a79184632774b94d
neha sable <nehasable45@gmail.com>
2024-07-12 13:10:36 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 
 import './App.css'
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Intermediate from './pages/Intermediate';
 function App() {
  
 
   return (
     <>
-    
+       <BrowserRouter>
+      <Routes>
+      <Route path="/intermediate" element={<Intermediate/>}/>
+       <Route path="/signin" element={<SignIn/>} />
+       <Route path="/signup" element={ <SignUp/>} />  
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+  
+    const handleLogin = async () => {
+      try {
+        const response = await axios.post("http://localhost:8000/auth/login", {
+          username,
+          password,
+        });
+        const { token, user } = response.data;
+        console.log(token);
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        // Redirect to dashboard or another page
+      } catch (err) {
+        setError(err.response.data.message);
+      }
+    };
   return (
     <div>
-      
+     <div>
+       <div className="login-container">
+      <h2>Sign Up</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='Role'
+      />
+      <Link to="/"><button onClick={handleLogin}>Login</button></Link>
+      {error && <p>{error}</p>}
+    </div>
+    </div> 
     </div>
   )
 }
